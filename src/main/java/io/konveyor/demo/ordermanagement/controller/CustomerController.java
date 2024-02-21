ca152678f6b8ad82fe5469d2d418de218d316ee6
Shawn Hurley <shawn@hurley.page>
2024-02-21 12:27:22 -0500

    adding use of removed java.xml.bind package

--- src/main/java/io/konveyor/demo/ordermanagement/controller/CustomerController.java
+++ src/main/java/io/konveyor/demo/ordermanagement/controller/CustomerController.java
@@ -1,9 +1,11 @@
 package io.konveyor.demo.ordermanagement.controller;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+
+import java.xml.bind.JAXB;
+
 import org.jboss.logging.Logger;
-import io.konveyor.demo.ordermanagement.exception.ResourceNotFoundException;
-import io.konveyor.demo.ordermanagement.model.Customer;
-import io.konveyor.demo.ordermanagement.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -12,6 +14,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.konveyor.demo.ordermanagement.exception.ResourceNotFoundException;
+import io.konveyor.demo.ordermanagement.model.Customer;
+import io.konveyor.demo.ordermanagement.service.CustomerService;
 
 @RestController
 @RequestMapping("/customers")
@@ -24,11 +30,15 @@ public class CustomerController {
 	
 	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Customer getById(@PathVariable("id") Long id) {
+		OutputStream opt = new ByteArrayOutputStream();
 		Customer c = customerService.findById(id);
 		if (c == null) {
 			throw new ResourceNotFoundException("Requested order doesn't exist");
 		}
+		JAXB.marshal(c, opt);
 		logger.debug("Returning element: " + c);
+
+
 		return c;
 	}
 	
