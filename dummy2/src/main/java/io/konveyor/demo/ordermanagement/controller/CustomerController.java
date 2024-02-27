54aca1507b264bcb62a1638aec29cbaa4eb27322
Shawn Hurley <shawn@hurley.page>
2024-02-27 15:13:13 -0500

    fixing mvn install to fail same-way

--- dummy2/src/main/java/io/konveyor/demo/ordermanagement/controller/CustomerController.java
+++ dummy2/src/main/java/io/konveyor/demo/ordermanagement/controller/CustomerController.java
@@ -1,10 +1,5 @@
 package io.konveyor.demo.ordermanagement.controller;
 
-import java.io.ByteArrayOutputStream;
-import java.io.OutputStream;
-
-import javax.xml.bind.JAXB;
-
 import org.jboss.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -15,7 +10,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.konveyor.demo.ordermanagement.exception.ResourceNotFoundException;
 import io.konveyor.demo.ordermanagement.model.Customer;
 import io.konveyor.demo.ordermanagement.service.CustomerService;
 
